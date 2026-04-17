5fac0beb9b1d1abeb590a525203a166b55dd73a2
nganne2212 <phamngan22122005@gmail.com>
2026-04-18 02:23:14 +0700

    ngân
    up

---
+++ qldsv/qldsv/Class/CurentUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qldsv.Class
+{
+    public static class CurrentUser
+    {
+        public static int MaNguoiDung { get; set; }
+        public static string TenDangNhap { get; set; }
+        public static string VaiTro { get; set; }
+        public static int MaDoiTuong { get; set; }
+        // MaSinhVien hoặc MaGiangVien tuỳ VaiTro
+    }
+}
--- qldsv/qldsv/Class/Functions.cs
+++ qldsv/qldsv/Class/Functions.cs
@@ -1,40 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Runtime.Remoting.Contexts;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
 
 namespace qldsv.Class
 {
     class Functions
     {
-        public static SqlConnection Conn;  //Khai báo đối tượng kết nối
-        public static string connString;   //Khai báo biến chứa chuỗi kết nối
+        public static SqlConnection Conn;
+        public static string connString;
 
         public static void Connect()
         {
-            //Thiết lập giá trị cho chuỗi kết nối
             connString = "Data Source=.\\SQLEXPRESS;Initial Catalog=qldiemsv;Integrated Security=True";
-            Conn = new SqlConnection();         		//Cấp phát đối tượng
-            Conn.ConnectionString = connString; 		//Kết nối
-            Conn.Open();                        		//Mở kết nối
-            MessageBox.Show("Kết nối thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Conn = new SqlConnection();
+            Conn.ConnectionString = connString;
+            Conn.Open();
         }
 
         public static void Disconnect()
         {
-            if (Conn.State == ConnectionState.Open)
+            if (Conn != null && Conn.State == ConnectionState.Open)
             {
-                Conn.Close();   	//Đóng kết nối
-                Conn.Dispose();     //Giải phóng tài nguyên
+                Conn.Close();
+                Conn.Dispose();
                 Conn = null;
             }
         }
 
+        public static string MaHoaMD5(string matKhau)
+        {
+            using (var md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(matKhau);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in hashBytes)
+                    sb.Append(b.ToString("x2"));
+                return sb.ToString();
+            }
+        }
     }
 }
