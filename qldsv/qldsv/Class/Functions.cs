1e4855dfb2772f9e66ffb87590df3cfddc6b14f7
nganne2212 <phamngan22122005@gmail.com>
2026-04-17 16:44:05 +0700

    Create Functions.cs
    up

---
+++ qldsv/qldsv/Class/Functions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace qldsv.Class
+{
+    class Functions
+    {
+        public static SqlConnection Conn;  //Khai báo đối tượng kết nối
+        public static string connString;   //Khai báo biến chứa chuỗi kết nối
+
+        public static void Connect()
+        {
+            //Thiết lập giá trị cho chuỗi kết nối
+            connString = "Data Source=.\\SQLEXPRESS;Initial Catalog=qldiemsv;Integrated Security=True";
+            Conn = new SqlConnection();         		//Cấp phát đối tượng
+            Conn.ConnectionString = connString; 		//Kết nối
+            Conn.Open();                        		//Mở kết nối
+            MessageBox.Show("Kết nối thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public static void Disconnect()
+        {
+            if (Conn.State == ConnectionState.Open)
+            {
+                Conn.Close();   	//Đóng kết nối
+                Conn.Dispose();     //Giải phóng tài nguyên
+                Conn = null;
+            }
+        }
+
+    }
+}
