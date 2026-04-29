ddd8a1d99061e40b870b55a26e29620205ffd4af
nganne2212 <phamngan22122005@gmail.com>
2026-04-29 12:42:19 +0700

    ngân

--- qldsv/qldsv/Forms/Sinhvien/FrmMainSinhVien.Designer.cs
+++ qldsv/qldsv/Forms/Sinhvien/FrmMainSinhVien.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmMainSinhVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FrmMainSinhVien";
             this.Text = "FrmMainSInhVien";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
